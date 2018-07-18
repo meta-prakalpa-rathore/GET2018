82617468b1be2dce64408360251db4e8db970588
prakalpa rathore <prakalpa.rathore@gmail.com>
2018-07-18 17:39:14 +0530

    Assignment-3

---
+++ assignment3/hexadecimalCalculator/Calculator.java
@@ -0,0 +1,18 @@
+package hexadecimalCalculator;
+
+public interface Calculator {
+
+	public String add(String number1, String number2);
+	
+	public String subtract(String number1, String number2);
+	
+	public String multiply(String number1, String number2);
+	
+	public String divide(String number1, String number2);
+	
+	public boolean equal(String number1, String number2);
+	
+	public boolean greaterThan(String number1, String number2);
+	
+	public boolean lessThan(String number1, String number2);
+}
