a952656e548d7e558400f66990569846b026d084
prakalpa rathore <prakalpa.rathore@gmail.com>
2018-07-31 10:47:18 +0530

    Assignment-9

---
+++ Assignment-9/assignment9/src/zooManagement/AnimalName.java
@@ -0,0 +1,10 @@
+package zooManagement;
+
+/**
+ * This enum represent the classes of animals which are present in the Zoo 
+ * @author Prakalpa-Rathore
+ */
+public enum AnimalName {
+
+	LION, ELEPHANT, CROCODILE, SNAKE, PEACOCK, PARROT;
+}
