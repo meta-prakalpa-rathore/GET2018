889e94d5abbdceeb2773f479b84dcb758a3d2b6d
prakalpa rathore <prakalpa.rathore@gmail.com>
2018-08-10 11:43:13 +0530

    DS Assignment 5

---
+++ DSAssignment5/DSAssignment#5/src/dictionary/BSTDictionary.java
@@ -0,0 +1,319 @@
+package dictionary;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * class to implement Dictionary using Binary Search Tree
+ * @author Prakalpa-Rathore
+ *
+ */
+public class BSTDictionary implements Dictionary
+{
+    private BSTNode root; //root node    
+    
+    /**
+     * constructor
+     * @param initialEntriesFileLocation, location of the json file having initial entries for the dictionary 
+     */
+    public BSTDictionary(String initialEntriesFileLocation)
+    {        
+        JSONParser parser = new JSONParser();
+        root = null;
+
+        try 
+        {
+            Object object = parser.parse(new FileReader(initialEntriesFileLocation));
+            JSONObject jsonObject = (JSONObject) object;
+            
+            //adding the initial entries from json file to BST dictionary
+            for(Object key: jsonObject.keySet()) 
+            {
+                DictionaryEntry newEntry = new DictionaryEntry(key.toString(), jsonObject.get(key).toString());
+                addEntry(newEntry);
+            }
+        }
+        catch(Exception exception)
+        {
+            System.out.println(exception);
+        }
+    }
+    
+    
+    /**
+     * method to add the given entry(key-value pair) into the dictionary
+     * @param entry to be added
+     * @return true if added, false otherwise
+     */
+    @Override
+    public boolean addEntry(DictionaryEntry entry)
+    {
+        try
+        {
+            if(entry == null)
+                throw new AssertionError("Invalid input");
+            
+            boolean inserted = false;
+            root = insert(root, entry);
+            if(root != null)
+                inserted = true;
+            
+            return inserted;
+        }
+        catch(AssertionError error)
+        {
+            return false;
+        }
+    }
+
+    
+    /**
+     * method to delete the given entry(key-value pair) from the dictionary
+     * @param key of the entry to be deleted
+     * @return true if deleted, false otherwise
+     */
+    @Override
+    public boolean deleteEntry(String key)
+    {        
+        try
+        {           
+            if(key == null)
+                throw new AssertionError("Invalid input");
+            
+            if (isEmpty())
+                throw new AssertionError("The dictionary is empty! Cannot delete");
+            
+            if (getValue(key) == null)
+                throw new AssertionError("The given key is not present in the dictionary");
+            
+            root = delete(root, key);
+                
+            return true;
+        }
+        catch(AssertionError error)
+        {
+            return false;
+        }
+    }
+
+    
+    /**
+     * method to return the value for the given key
+     * @param key for which value is to be returned
+     * @return value of the key if key is present else returns null
+     */
+    @Override
+    public String getValue(String key)
+    {        
+        try
+        {
+            if(key == null)
+                throw new AssertionError("Invalid input");
+            
+            if (isEmpty())
+                throw new AssertionError("The dictionary is empty!");
+            
+            String value = getValue(root, key);
+            
+            if(value == null)
+                throw new AssertionError("The given key is not present in the dictionary");
+            
+            return value;
+        }
+        catch(AssertionError error)
+        {
+            return null;
+        }
+    }
+
+    
+    /**
+     * returns all the entries in ascending order of key
+     */
+    @Override
+    public List<DictionaryEntry> getSortedList()
+    {        
+        List<DictionaryEntry> sortedEntryList = new ArrayList<DictionaryEntry>();
+        inorderTraversal(root, sortedEntryList);
+        
+        return sortedEntryList;
+    }
+
+    
+    /**
+     * return the sorted list of entries for all the keys >=key1 and <=key2
+     * @param key1
+     * @param key2
+     * @return sorted list, returns null if input is invalid
+     */
+    @Override
+    public List<DictionaryEntry> getSortedList(String key1, String key2)
+    {
+        try
+        {
+            if(key1 == null || key2 == null)
+                throw new AssertionError("invalid input");
+            
+            List<DictionaryEntry> sortedSubList = new ArrayList<DictionaryEntry>();
+            List<DictionaryEntry> sortedEntryList = getSortedList();
+            
+            Iterator<DictionaryEntry> iterator = sortedEntryList.iterator();
+            
+            while(iterator.hasNext())
+            {
+                DictionaryEntry entry = iterator.next();
+                if(key1.compareTo(entry.getKey()) < 0 && key2.compareTo(entry.getKey()) > 0)
+                {
+                    sortedSubList.add(entry);
+                }
+            }
+            
+            return sortedSubList;
+        }
+        catch(AssertionError error)
+        {
+            return null;
+        }
+    }
+    
+    
+    /**
+     * method to check if the dictionary is empty
+     * @return boolean value
+     */
+    public boolean isEmpty()
+    {
+        return root == null;
+    }
+    
+    
+    /**
+     * helper method for inserting entries in dictionary using recursion
+     * @param root
+     * @param entry
+     * @return reference of the root of the current tree or subtree
+     * @throws AssertionError if there is a duplicate entry
+     */
+    private BSTNode insert(BSTNode root, DictionaryEntry entry) throws AssertionError
+    {
+        if (root == null)
+            root = new BSTNode(entry);
+        else
+        {
+            if (entry.getKey().compareTo(root.getData().getKey()) == 0)
+                throw new AssertionError("Duplicate entry");
+            else if (entry.getKey().compareTo(root.getData().getKey()) < 0)
+                root.left = insert(root.left, entry);
+            else
+                root.right = insert(root.right, entry);
+        }
+        
+        return root;
+    }
+    
+    
+    /**
+     * helper method to delete an entry from the dictionary using recursion
+     * @param root
+     * @param key of the entry to be deleted
+     * @return reference of the root of the current tree or subtree
+     */
+    private BSTNode delete(BSTNode root, String key)
+    {
+        BSTNode newRoot, node;
+        
+        if (root.getData().getKey().equals(key))
+        {
+            BSTNode left, right;
+            left = root.getLeft();
+            right = root.getRight();
+
+            if (left == null && right == null)
+                return null;
+            else if (left == null)
+            {
+                newRoot = right;
+                return newRoot;
+            }
+            else if (right == null)
+            {
+                newRoot = left;
+                return newRoot;
+            }
+            else
+            {
+                newRoot = right;
+                BSTNode leftmost = right;
+                
+                while (leftmost.getLeft() != null)
+                    leftmost = leftmost.getLeft();
+
+                leftmost.setLeft(left);
+                return newRoot;
+            }
+        }
+
+        if (key.compareTo(root.getData().getKey()) < 0)
+        {
+            node = delete(root.getLeft(), key);
+            root.setLeft(node);
+        }
+        else
+        {
+            node = delete(root.getRight(), key);
+            root.setRight(node);             
+        }
+        
+        return root;
+    }
+    
+    
+    /**
+     * helper method to get the value of the given key using recursion
+     * @param root
+     * @param key
+     * @return value 
+     */
+    private String getValue(BSTNode root, String key)
+    {
+        String value = null;
+
+        while ((root != null) && value == null)
+        {            
+            if (key.compareTo(root.getData().getKey()) < 0)
+                root = root.getLeft();
+            else if (key.compareTo(root.getData().getKey()) > 0)
+                root = root.getRight();
+            else
+            {
+                value = root.getData().getValue();
+                break;
+            }
+            
+            value = getValue(root, key);
+        }
+
+        return value;
+    }
+    
+    
+    /**
+     * helper method which adds all the entries from dictionary to a list using inorder traversal 
+     * @param root of the BST Dictionary
+     */
+    private void inorderTraversal(BSTNode root, List<DictionaryEntry> sortedEntryList)
+    {
+        if (root != null)
+        {
+            inorderTraversal(root.getLeft(), sortedEntryList);
+            sortedEntryList.add(root.getData());
+            inorderTraversal(root.getRight(), sortedEntryList);
+        }
+    }
+}
---
+++ DSAssignment5/DSAssignment#5/src/dictionary/DictionaryTest.java
@@ -0,0 +1,125 @@
+package dictionary;
+
+import static org.junit.Assert.*;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.junit.Test;
+
+public class DictionaryTest {
+
+    /**
+     * test case for addEntry
+     */
+    @Test
+    public void addEntryTest() {
+        
+        Dictionary dictionary = new BSTDictionary("D:\\eclipse workspace\\DSAssignment#5\\test.json");
+        
+        assertTrue(dictionary.addEntry(new DictionaryEntry("hi", "hello")));
+        assertTrue(dictionary.addEntry(new DictionaryEntry("prakalpa", "rathore")));
+        assertTrue(dictionary.addEntry(new DictionaryEntry("abc", "hello")));
+        assertFalse(dictionary.addEntry(new DictionaryEntry("hi", "world"))); //duplicate key
+        assertFalse(dictionary.addEntry(null)); //null input
+    }
+    
+    
+    /**
+     * test case for deleteEntry
+     */
+    @Test
+    public void deleteEntryTest() {
+        
+        Dictionary dictionary = new BSTDictionary("D:\\eclipse workspace\\DSAssignment#5\\test.json");
+        
+        dictionary.addEntry(new DictionaryEntry("hi", "hello"));
+        dictionary.addEntry(new DictionaryEntry("prakalpa", "rathore"));
+        dictionary.addEntry(new DictionaryEntry("abc", "hello"));
+        
+        assertTrue(dictionary.deleteEntry("hi"));
+        assertTrue(dictionary.deleteEntry("prakalpa"));
+        assertFalse(dictionary.deleteEntry("hi")); //key is already deleted
+        assertFalse(dictionary.deleteEntry("mango")); //key does not exist
+        assertFalse(dictionary.deleteEntry(null)); //key is null
+    }
+    
+    
+    /**
+     * test case for getValue
+     */
+    @Test
+    public void getValueTest() {
+        
+        Dictionary dictionary = new BSTDictionary("D:\\eclipse workspace\\DSAssignment#5\\test.json");
+        
+        assertEquals("a", dictionary.getValue("good"));
+        assertEquals("b", dictionary.getValue("rat"));
+        assertEquals(null, dictionary.getValue(null)); //key is null
+        assertEquals(null, dictionary.getValue("hi")); //key does not exist
+        
+        dictionary.deleteEntry("good");
+        dictionary.deleteEntry("rat");
+        dictionary.deleteEntry("hire");
+        assertEquals(null, dictionary.getValue("good")); //dictionary is empty
+    }
+    
+    
+    /**
+     * test case to get all the entries in sorted form
+     */
+    @Test
+    public void sortedListTest1() {
+        
+        Dictionary dictionary = new BSTDictionary("D:\\eclipse workspace\\DSAssignment#5\\test.json");
+        
+        dictionary.addEntry(new DictionaryEntry("hi", "hello"));
+        dictionary.addEntry(new DictionaryEntry("prakalpa", "rathore"));
+        dictionary.addEntry(new DictionaryEntry("abc", "hello"));
+     
+        
+        List<DictionaryEntry> sortedList = dictionary.getSortedList();
+        List<DictionaryEntry> expectedList = new ArrayList<DictionaryEntry>();
+        expectedList.add(new DictionaryEntry("abc", "hello"));
+        expectedList.add(new DictionaryEntry("good", "a"));
+        expectedList.add(new DictionaryEntry("hi", "hello"));
+        expectedList.add(new DictionaryEntry("hire", "c"));
+        expectedList.add(new DictionaryEntry("prakalpa", "rathore"));
+        expectedList.add(new DictionaryEntry("rat", "b"));
+        
+        for(int i = 0; i < sortedList.size(); i++)
+            assertEquals(expectedList.get(i).getKey(), sortedList.get(i).getKey());
+        
+    }
+    
+    
+    /**
+     * test case to get entries in sorted form between key1 and key2
+     */
+    @Test
+    public void sortedListTest2() {
+        
+        Dictionary dictionary = new BSTDictionary("D:\\eclipse workspace\\DSAssignment#5\\test.json");
+        dictionary.addEntry(new DictionaryEntry("hi", "hello"));
+        dictionary.addEntry(new DictionaryEntry("prakalpa", "rathore"));
+        dictionary.addEntry(new DictionaryEntry("abc", "hello"));
+        
+        List<DictionaryEntry> sortedList = dictionary.getSortedList("a", "r");
+        List<DictionaryEntry> expectedList = new ArrayList<DictionaryEntry>();
+        expectedList.add(new DictionaryEntry("abc", "hello"));
+        expectedList.add(new DictionaryEntry("good", "a"));
+        expectedList.add(new DictionaryEntry("hi", "hello"));
+        expectedList.add(new DictionaryEntry("hire", "c"));
+        expectedList.add(new DictionaryEntry("prakalpa", "rathore"));        
+        
+        for(int i = 0; i < sortedList.size(); i++)
+            assertEquals(expectedList.get(i).getKey(), sortedList.get(i).getKey());
+    }
+        
+}
+    
