7b2f6bd59a590238658c5c6e6548620bbbc82fa9
meta-prakalpa-rathore <41181574+meta-prakalpa-rathore@users.noreply.github.com>
2018-08-14 12:24:46 +0530

    Update GraphImplementationTest.java

--- DSAssignment6/Graph/src/graph/GraphImplementationTest.java
+++ DSAssignment6/Graph/src/graph/GraphImplementationTest.java
@@ -2,14 +2,18 @@ package graph;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
 public class GraphImplementationTest {
 
+    /**
+     * test case for isConnected when graph is connected
+     */
     @Test
-    public void test() {
+    public void isConnectedTest1() {
         
         GraphImplementation graph = new GraphImplementation(4);
         graph.addEdge(0, 1, 2);
@@ -21,8 +25,11 @@ public class GraphImplementationTest {
     }
 
     
+    /**
+     * test case for isConnected when graph is not connected
+     */
     @Test
-    public void test1() {
+    public void isConnectedTest2() {
         
         GraphImplementation graph = new GraphImplementation(6);
         graph.addEdge(0, 1, 2);
@@ -35,8 +42,11 @@ public class GraphImplementationTest {
     }
     
     
+    /**
+     * test case for reachable
+     */
     @Test
-    public void test2() {
+    public void reachableTest() {
         
         GraphImplementation graph = new GraphImplementation(6);
         graph.addEdge(0, 1, 2);
@@ -45,15 +55,31 @@ public class GraphImplementationTest {
         graph.addEdge(3, 0, 4);
         graph.addEdge(4, 5, 4);
         
-        List<Integer> reachable = graph.reachable(5);
+        List<Integer> expected = new ArrayList<Integer>();
+        expected.add(4);
         
-        for(Integer i: reachable)
-            System.out.println(i);
+        assertEquals(expected, graph.reachable(5));
+        
+        
+        graph.addEdge(3, 4, 1);
+        
+        List<Integer> newExpected = new ArrayList<Integer>();
+        newExpected.add(0);
+        newExpected.add(1);
+        newExpected.add(2);
+        newExpected.add(3);
+        newExpected.add(4);
+        
+        assertEquals(newExpected, graph.reachable(5));
+             
     }
     
     
+    /**
+     * test case for shortestPath
+     */
     @Test
-    public void test3() {
+    public void shortestPathTest() {
         
         GraphImplementation graph = new GraphImplementation(4);
         graph.addEdge(0, 1, 2);
@@ -65,8 +91,12 @@ public class GraphImplementationTest {
         
     }
     
+    
+    /**
+     * test case for minimumSpanningTree
+     */
     @Test
-    public void test4() {
+    public void minimumSpanningTreeTest() {
         
         GraphImplementation graph = new GraphImplementation(4);
         graph.addEdge(0, 1, 2);
@@ -75,8 +105,18 @@ public class GraphImplementationTest {
         graph.addEdge(3, 0, 4);
         graph.addEdge(0, 2, 2);
         
-        List<Edge> g = graph.minimumSpanningTree();
-        for(Edge e: g)
-        	System.out.println(e.getSource() + " " + e.getDestination() + " " + e.getWeight());
+        List<Edge> expected = new ArrayList<Edge>();
+        expected.add(new Edge(0, 1, 2));
+        expected.add(new Edge(0, 2, 2));
+        expected.add(new Edge(0, 3, 4));
+        
+        List<Edge> mst = graph.minimumSpanningTree();
+        
+        for(int i = 0; i < mst.size(); i++)
+        {
+            assertEquals(expected.get(i).getSource(), mst.get(i).getSource());
+            assertEquals(expected.get(i).getDestination(), mst.get(i).getDestination());
+            assertEquals(expected.get(i).getWeight(), mst.get(i).getWeight());
+        }
     }
 }
