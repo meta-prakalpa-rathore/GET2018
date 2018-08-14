4cf42858553b769ebc4c752916faa26f2e9da410
meta-prakalpa-rathore <41181574+meta-prakalpa-rathore@users.noreply.github.com>
2018-08-14 12:23:34 +0530

    Update Graph.java

--- DSAssignment6/Graph/src/graph/Graph.java
+++ DSAssignment6/Graph/src/graph/Graph.java
@@ -2,13 +2,42 @@ package graph;
 
 import java.util.List;
 
-public interface Graph {
 
+/**
+ * interface for an  undirected weighted graph
+ * @author Prakalpa-Rathore
+ *
+ */
+public interface Graph 
+{
+    
+    /**
+     * method to check whether the graph is connected or not
+     * @return boolean value
+     */
     public boolean isConnected();
     
+    
+    /**
+     * method to find all the vertices which are reachable from the source vertex
+     * @param source vertex
+     * @return list of reachable vertices
+     */
     public List<Integer> reachable(int source);
     
+    
+    /**
+     * method to create the minimum spanning tree 
+     * @return list of edges in minimum spanning tree
+     */
     public List<Edge> minimumSpanningTree();
     
+    
+    /**
+     * method to find the distance of the shortest path from source to destination
+     * @param source
+     * @param destination
+     * @return distance of shortest path
+     */
     public int shortestPath(int source, int destination);
 }
