18e6bf9b2dc243bffa1ebc6490195ba3e8c0d9f6
meta-prakalpa-rathore <41181574+meta-prakalpa-rathore@users.noreply.github.com>
2018-08-20 14:29:57 +0530

    Add files via upload

--- DBMS-Assignment-3/queryS3A4.sql
+++ DBMS-Assignment-3/queryS3A4.sql
@@ -1,17 +1,65 @@
-/*Consider a form where providing a Zip Code populates associated City and State.*/
+/*Consider a form where providing a Zip Code populates associated City and
+State.*/
 
 /*Create appropriate tables and relationships for the same and write a SQL
 query for that returns a Resultset containing Zip Code, City Names and
 States ordered by State Name and City Name.*/
-CREATE TABLE zipcode(
-pin_code INT PRIMARY KEY,
-city VARCHAR(25) NOT NULL ,
-state VARCHAR(25) NOT NULL 
+USE storefront;
+
+CREATE TABLE state(
+state_id INTEGER AUTO_INCREMENT,
+state_name VARCHAR(20),
+PRIMARY KEY(state_id)
 );
-INSERT INTO zipcode(pin_code,city,state)
-VALUES(484001,"shahdol","mp"),
-      (302022,"jaipur","raj"),
-      (313002,"udaipur","raj");
-SELECT pin_code , city , state
-FROM zipcode
-ORDER BY state,city;
+
+CREATE TABLE city(
+city_id INTEGER AUTO_INCREMENT,
+city_name VARCHAR(20),
+state_id INTEGER,
+PRIMARY KEY(city_id),
+FOREIGN KEY(state_id) REFERENCES state(state_id)
+);
+
+CREATE TABLE zipcode(
+zipcode CHAR(6),
+city_id INTEGER,
+PRIMARY KEY(zipcode),
+FOREIGN KEY(city_id) REFERENCES city(city_id)
+);
+
+INSERT INTO state(state_name)
+VALUES("rajasthan"),
+	  ("haryana"),
+      ("Uttar Pradesh"),
+      ("Madhya Pradesh");
+      
+INSERT INTO city(city_name,state_id)
+VALUES("udaipur", 1),
+	  ("jaipur", 1),
+      ("faridabad", 2),
+      ("rohtak", 2),
+      ("agra", 3),
+      ("jhansi", 3),
+      ("shahdol", 4),
+      ("gwalior", 4);
+	
+INSERT INTO zipcode(zipcode,city_id)
+VALUES("313002", 1),
+	  ("313003", 1),
+      ("302002", 2),
+      ("302003", 2),
+      ("121003", 3),
+      ("124001", 4),
+      ("124021", 4),
+      ("223007", 5),
+      ("282005", 5),
+      ("284001", 6),
+      ("284002", 6),
+      ("484001", 7),
+      ("474005", 8);
+      
+SELECT z.zipcode , c.city_name , s.state_name
+FROM zipcode z, city c, state s
+WHERE z.zipcode = "121003" 
+AND z.city_id = c.city_id
+AND s.state_id = c.state_id;
