48c22588a2b9c597227c620f2ae507e80b2d5b27
meta-prakalpa-rathore <41181574+meta-prakalpa-rathore@users.noreply.github.com>
2018-08-20 23:43:39 +0530

    DBMS-Assignment-4

--- queryS4A3.sql
+++ queryS4A3.sql
@@ -1,3 +1,5 @@
 ALTER TABLE orders ADD INDEX orderTableIndex(order_id);
+
 ALTER TABLE product ADD INDEX productTableIndex(product_id);
+
 ALTER TABLE category ADD INDEX categoryTableIndex(category_id);
