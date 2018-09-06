3c4de56b39582af566c9caab7fcc23eeaf937a67
meta-prakalpa-rathore <41181574+meta-prakalpa-rathore@users.noreply.github.com>
2018-09-06 10:48:49 +0530

    EAD-Assignment-7

--- EAD-Assignment-7/1.1-SpringExConstructorInjection/src/com/metacube/training/MainApp.java
+++ EAD-Assignment-7/1.1-SpringExConstructorInjection/src/com/metacube/training/MainApp.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * @author User5
+ * @author Prakalpa-Rathore
  *
  */
 public class MainApp {
