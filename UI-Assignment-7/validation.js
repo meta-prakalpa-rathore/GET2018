104b5e473b2d53d9e2d6b5d09c2ca4751f287f0e
prakalpa rathore <prakalpa.rathore@gmail.com>
2018-10-04 19:09:28 +0530

    UI-Assignment-7

--- UI-Assignment-7/validation.js
+++ UI-Assignment-7/validation.js
@@ -1,24 +1,24 @@
 var errors = new Map();
 
 function firstNameValidate() {
-	var text = document.getElementById("fname");
+	var firstNameElement = document.getElementById("fname");
 	var nameRegEx = /^[a-zA-Z\s]*$/;
     
-    if(text.value == "") {
-        text.style.border = "solid thin red";
+    if(firstNameElement.value == "") {
+        firstNameElement.style.border = "solid thin red";
 		fnameErr.innerHTML = "This field is required";
         errors.set("First Name", "First Name is required");
 		return false;
     }
-	else if (text.value.length < 2 || !nameRegEx.test(text.value)) {
-		text.style.border = "solid thin red";
+	else if (firstNameElement.value.length < 2 || !nameRegEx.test(firstNameElement.value)) {
+		firstNameElement.style.border = "solid thin red";
 		fnameErr.innerHTML = "Invalid first name";
         errors.set("First Name", "Invalid first name");
 		return false;
 	}
 	else {
 		fnameErr.innerHTML = "";
-		text.style.borderColor = "";
+		firstNameElement.style.borderColor = "";
         
         if(errors.has("First Name")) {
             errors.delete("First Name");
@@ -28,24 +28,24 @@ function firstNameValidate() {
 }
 			
 function lastNameValidate() {
-	var text = document.getElementById("lname");
+	var lastNameElement = document.getElementById("lname");
 	var nameRegEx = /^[a-zA-Z]*$/;
     
-	if(text.value == "") {
-        text.style.border = "solid thin red";
+	if(lastNameElement.value == "") {
+        lastNameElement.style.border = "solid thin red";
 		lnameErr.innerHTML = "This field is required";
         errors.set("Last Name", "Last Name is required");
 		return false;
     }
-	else if (text.value.length < 2 || !nameRegEx.test(text.value)) {
-		text.style.border = "solid thin red";
+	else if (lastNameElement.value.length < 2 || !nameRegEx.test(lastNameElement.value)) {
+		lastNameElement.style.border = "solid thin red";
 		lnameErr.innerHTML = "Invalid last name";
         errors.set("Last Name", "Invalid last name");
 		return false;
 	}
 	else {
 		lnameErr.innerHTML = "";
-		text.style.borderColor = "";
+		lastNameElement.style.borderColor = "";
         
         if(errors.has("Last Name")) {
             errors.delete("Last Name");
@@ -56,24 +56,24 @@ function lastNameValidate() {
 }
 
 function emailValidate() {
-	var text = document.getElementById("email");
+	var emailElement = document.getElementById("email");
 	var emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
-	if(text.value == "") {
-        text.style.border = "solid thin red";
+	if(emailElement.value == "") {
+        emailElement.style.border = "solid thin red";
 		emailErr.innerHTML = "This field is required";
         errors.set("E-Mail", "E-Mail is required");
 		return false;
     }
-	else if (!emailRegEx.test(text.value)) {
-		text.style.border = "solid thin red";
+	else if (!emailRegEx.test(emailElement.value)) {
+		emailElement.style.border = "solid thin red";
 		emailErr.innerHTML = "Invalid email";
         errors.set("E-Mail", "Invalid email");
 		return false;
 	}
 	else {
 		emailErr.innerHTML = "";
-		text.style.borderColor = "";
+		emailElement.style.borderColor = "";
         
         if(errors.has("E-Mail")) {
             errors.delete("E-Mail");
@@ -84,11 +84,11 @@ function emailValidate() {
 }
 
 function phoneValidate() {
-	var text = document.getElementById("phone");
+	var phoneElement = document.getElementById("phone");
 	var contactRegEx = /^[0-9]*$/;
     
-    if(text.value == "") {
-        text.style.border = "";
+    if(phoneElement.value == "") {
+        phoneElement.style.border = "";
 		phoneErr.innerHTML = "";
         
         if(errors.has("Phone")) {
@@ -97,15 +97,15 @@ function phoneValidate() {
         
 		return true;
     }
-	else if (text.value.length < 8 || !contactRegEx.test(text.value)){
-		text.style.border = "solid thin red";
+	else if (phoneElement.value.length < 8 || !contactRegEx.test(phoneElement.value)){
+		phoneElement.style.border = "solid thin red";
 		phoneErr.innerHTML = "Invalid contact number";
         errors.set("Phone", "Invalid contact number");
 		return false;
 	}
 	else {
 		phoneErr.innerHTML = "";
-		text.style.borderColor = "";
+		phoneElement.style.borderColor = "";
         
         if(errors.has("Phone")) {
             errors.delete("Phone");
@@ -116,11 +116,11 @@ function phoneValidate() {
 }
 
 function cityValidate() {
-	var text = document.getElementById("city");
+	var cityElement = document.getElementById("city");
 	var nameRegEx = /^[a-zA-Z\s]*$/;
     
-	if(text.value == "") {
-        text.style.border = "";
+	if(cityElement.value == "") {
+        cityElement.style.border = "";
 		cityErr.innerHTML = "";
         
         if(errors.has("City")) {
@@ -129,15 +129,15 @@ function cityValidate() {
         
 		return true;
     }
-	else if (text.value.length < 2 || !nameRegEx.test(text.value)) {
-		text.style.border = "solid thin red";
+	else if (cityElement.value.length < 2 || !nameRegEx.test(cityElement.value)) {
+		cityElement.style.border = "solid thin red";
 		cityErr.innerHTML = "Invalid city";
         errors.set("City", "Invalid City");
 		return false;
 	}
 	else {
 		cityErr.innerHTML = "";
-		text.style.borderColor = "";
+		cityElement.style.borderColor = "";
         
         if(errors.has("City")) {
             errors.delete("City");
@@ -148,17 +148,17 @@ function cityValidate() {
 }
 
 function stateValidate() {
-    var text = document.getElementById("state");
+    var stateElement = document.getElementById("state");
     
-    if (text.value == "") {
-        text.style.border = "solid thin red";
+    if (stateElement.value == "") {
+        stateElement.style.border = "solid thin red";
 		stateErr.innerHTML = "This field is required";
         errors.set("State", "State is required");
 		return false;
     }
     else {
         stateErr.innerHTML = "";
-        text.style.borderColor = "";
+        stateElement.style.borderColor = "";
         
         if(errors.has("State")) {
             errors.delete("State");
@@ -169,17 +169,17 @@ function stateValidate() {
 }
 
 function zipcodeValidate() {
-    var text = document.getElementById("zipcode");
+    var zipcodeElement = document.getElementById("zipcode");
     
-    if (text.value == "") {
-        text.style.border = "solid thin red";
+    if (zipcodeElement.value == "") {
+        zipcodeElement.style.border = "solid thin red";
 		zipcodeErr.innerHTML = "This field is required";
         errors.set("Zip Code", "Zip Code is required");
 		return false;
     }
     else {
         zipcodeErr.innerHTML = "";
-        text.style.borderColor = "";
+        zipcodeElement.style.borderColor = "";
         
         if(errors.has("Zip Code")) {
             errors.delete("Zip Code");
@@ -190,17 +190,17 @@ function zipcodeValidate() {
 }
 
 function projectValidate() {
-    var text = document.getElementById("project_description");
+    var projectElement = document.getElementById("project_description");
     
-    if (text.value == "") {
-        text.style.border = "solid thin red";
+    if (projectElement.value == "") {
+        projectElement.style.border = "solid thin red";
 		projectErr.innerHTML = "This field is required";
         errors.set("Project Description", "Project Description is required");
 		return false;
     }
     else {
         projectErr.innerHTML = "";
-        text.style.borderColor = "";
+        projectElement.style.borderColor = "";
         
         if(errors.has("Project Description")) {
             errors.delete("Project Description");
@@ -211,11 +211,11 @@ function projectValidate() {
 }
 
 function websiteValidate() {
-	var text = document.getElementById("website");
+	var websiteElement = document.getElementById("website");
 	var websiteRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
     
-    if(text.value == "") {
-        text.style.border = "";
+    if(websiteElement.value == "") {
+        websiteElement.style.border = "";
 		websiteErr.innerHTML = "";
         
         if(errors.has("Website or domain name")) {
@@ -224,15 +224,15 @@ function websiteValidate() {
         
 		return true;
     }
-	else if (!websiteRegEx.test(text.value)){
-		text.style.border = "solid thin red";
+	else if (!websiteRegEx.test(websiteElement.value)){
+		websiteElement.style.border = "solid thin red";
 		websiteErr.innerHTML = "Invalid website or domain name";
         errors.set("Website or domain name", "Invalid Website or domain name");
 		return false;
 	}
 	else {
 		websiteErr.innerHTML = "";
-		text.style.borderColor = "";
+		websiteElement.style.borderColor = "";
         
         if(errors.has("Website or domain name")) {
             errors.delete("Website or domain name");
@@ -242,56 +242,68 @@ function websiteValidate() {
 	}
 }
 
-function stateChange() {
-    var text = document.getElementById("state");
-    var referenceNode = text.parentNode.parentNode.parentNode;
-    
-    var zipcode = document.createElement('div');
-        zipcode.className = "form-element";
-        zipcode.innerHTML = "<div class='input-label'>Zip Code<span>*</span></div><div class='input-group'><div class='icon'><i class='fa fa-home'></i></div><div class='input-field'><input type='text' id='zipcode' placeholder='Zip Code' onblur='zipcodeValidate()'><p id='zipcodeErr'></p></div></div>";
-    
-    var website = document.createElement('div');
-        website.className = "form-element";
-        website.innerHTML = "<div class='input-label'>Website or domain name</div><div class='input-group'><div class='icon'><i class='fa fa-globe'></i></div><div class='input-field'><input type='text' id='website' placeholder='Website or domain name' onblur='websiteValidate()'><p id='websiteErr'></p></div></div>";
-    
-    var hosting = document.createElement('div');
-        hosting.className = "form-element";
-        hosting.innerHTML = "<div class='input-label'>Do you have hosting?</div><div class='input-group'><div class='input-radio-field'><label class='radio-label'><input type='radio' id='yes_hosting' value='yes'> Yes</label><label class='radio-label'><input type='radio' id='no_hosting' value='no'> No</label></div></div>";
-    
-    var project = document.createElement('div');
-        project.className = "form-element";
-        project.innerHTML = "<div class='input-label'>Project Description<span>*</span></div><div class='input-group'><div class='icon'><i class='fa fa-pencil'></i></div><div class='input-field'><textarea id='project_description' rows='3' placeholder='Project Description' onblur='projectValidate()'></textarea><p id='projectErr'></p></div></div>";
-    
-    var countChildren = document.getElementById("contactForm").children.length;
-    
-    if(countChildren == 10) {
-        var parentForm = document.getElementById("contactForm");
-        var item1 = parentForm.childNodes[14];
-        var item2 = parentForm.childNodes[15];
-        
-        parentForm.removeChild(item1);
-        parentForm.removeChild(item2);
+function handleStateChange() {
+    var stateElement = document.getElementById("state");
+    var state = stateElement.value;
+    var referenceNode = stateElement.parentNode.parentNode.parentNode;    
+    var optionalElements = document.getElementsByClassName("optionalElement");
+   
+    while (optionalElements.length > 0) {
+        optionalElements[0].parentNode.removeChild(optionalElements[0]);
     }
                           
-    if(text.value == "Rajasthan") {
-        referenceNode.parentNode.insertBefore(project, referenceNode.nextSibling);
+    if(state == "Rajasthan") {
+        referenceNode.parentNode.insertBefore(getProject(), referenceNode.nextSibling);
         
-        referenceNode.parentNode.insertBefore(website, referenceNode.nextSibling);
+        referenceNode.parentNode.insertBefore(getWebsite(), referenceNode.nextSibling);
     }
-    else if(text.value == "Haryana") {
-        referenceNode.parentNode.insertBefore(hosting, referenceNode.nextSibling);
+    else if(state == "Haryana") {
+        referenceNode.parentNode.insertBefore(getHosting(), referenceNode.nextSibling);
         
-        referenceNode.parentNode.insertBefore(zipcode, referenceNode.nextSibling);
+        referenceNode.parentNode.insertBefore(getZipCode(), referenceNode.nextSibling);
     }
-    else if(text.value == "Maharashtra") {
-        referenceNode.parentNode.insertBefore(project, referenceNode.nextSibling);
+    else if(state == "Maharashtra") {
+        referenceNode.parentNode.insertBefore(getProject(), referenceNode.nextSibling);
         
-        referenceNode.parentNode.insertBefore(zipcode, referenceNode.nextSibling);
+        referenceNode.parentNode.insertBefore(getZipCode(), referenceNode.nextSibling);
     }
+}
+
+function getWebsite() {
+    var website = document.createElement('div');
+        website.className = "form-element optionalElement";
+        website.innerHTML = "<div class='input-label'>Website or domain name</div><div class='input-group'><div class='icon'><i class='fa fa-globe'></i></div><div class='input-field'><input type='text' id='website' placeholder='Website or domain name' onblur='websiteValidate()'><p id='websiteErr'></p></div></div>";
+    
+    return website;
+}
+
+function getHosting() {
+    var hosting = document.createElement('div');
+        hosting.className = "form-element optionalElement";
+        hosting.innerHTML = "<div class='input-label'>Do you have hosting?</div><div class='input-group'><div class='input-radio-field'><label class='radio-label'><input type='radio' name='has_hosting' id='yes_hosting' value='yes'> Yes</label><label class='radio-label'><input type='radio' name='has_hosting' id='no_hosting' value='no'> No</label></div></div>";
+    
+    return hosting;
+}
+
+function getZipCode() {
+    var zipcode = document.createElement('div');
+        zipcode.className = "form-element optionalElement";
+        zipcode.innerHTML = "<div class='input-label'>Zip Code<span>*</span></div><div class='input-group'><div class='icon'><i class='fa fa-home'></i></div><div class='input-field'><input type='text' id='zipcode' placeholder='Zip Code' onblur='zipcodeValidate()'><p id='zipcodeErr'></p></div></div>";
+    
+    return zipcode;
+}
+
+function getProject() {
+    var project = document.createElement('div');
+        project.className = "form-element optionalElement";
+        project.innerHTML = "<div class='input-label'>Project Description<span>*</span></div><div class='input-group'><div class='icon'><i class='fa fa-pencil'></i></div><div class='input-field'><textarea id='project_description' rows='3' placeholder='Project Description' onblur='projectValidate()'></textarea><p id='projectErr'></p></div></div>";
+    
+    return project;
 }
 
 function submitForm(){
     
+    errors.clear();
     var state = document.getElementById("state");
     var flag = false;
     
@@ -312,6 +324,7 @@ function submitForm(){
         localStorage.setItem("First Name", document.getElementById("fname").value);
         localStorage.setItem("Last Name", document.getElementById("lname").value);
         localStorage.setItem("Email", document.getElementById("email").value);
+        localStorage.setItem("State", document.getElementById("state").value);
         
         var phoneNumber = document.getElementById("phone");
         if(phoneNumber.value != "") {
@@ -327,35 +340,30 @@ function submitForm(){
         if(city.value != "") {
             localStorage.setItem("City", city.value);
         }
+                    
+        var project = document.getElementById("project_description");
+        if(project != null) {
+            localStorage.setItem("Project Description", project.value);
+        }            
         
-        var state = document.getElementById("state");
-        localStorage.setItem("State", state.value);
+        var website = document.getElementById("website");
+        if(website != null && website.value != "") {
+            localStorage.setItem("Website or domain name", website.value);
+        }
         
-        if(state.value == "Rajasthan") {
-            localStorage.setItem("Project Description", document.getElementById("project_description").value);
-            
-            var website = document.getElementById("website");
-            if(website.value != "") {
-                localStorage.setItem("Website or domain name", website.value);
-            }
+        var zipcode = document.getElementById("zipcode");
+        if(zipcode != null) {
+            localStorage.setItem("Zip Code", zipcode.value);
         }
-        else if(state.value == "Haryana") {
-            localStorage.setItem("Zip Code", document.getElementById("zipcode").value);
-            
-            var yesHosting = document.getElementById("yes_hosting");
-            var noHosting = document.getElementById("no_hosting");
-            
-            if(yesHosting.checked) {
-                localStorage.setItem("Has Hosting", yesHosting.value);
-            }
-            else if(noHosting.checked) {
-                localStorage.setItem("Has Hosting", noHosting.value);
-            }
+        
+        var yesHosting = document.getElementById("yes_hosting");
+        var noHosting = document.getElementById("no_hosting");
+
+        if(yesHosting != null && yesHosting.checked) {
+            localStorage.setItem("Has Hosting", yesHosting.value);
         }
-        else {
-            localStorage.setItem("Zip Code", document.getElementById("zipcode").value);
-            
-            localStorage.setItem("Project Description", document.getElementById("project_description").value);
+        else if(noHosting != null && noHosting.checked) {
+            localStorage.setItem("Has Hosting", noHosting.value);
         }
         
         return true;
