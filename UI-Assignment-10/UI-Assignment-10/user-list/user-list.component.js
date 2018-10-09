4138475b5a63ff3dc1ec07ef8032b26d50c72448
prakalpa rathore <prakalpa.rathore@gmail.com>
2018-10-09 19:19:47 +0530

    UI-Assignment-10

--- UI-Assignment-10/UI-Assignment-10/user-list/user-list.component.js
+++ UI-Assignment-10/UI-Assignment-10/user-list/user-list.component.js
@@ -4,40 +4,37 @@ angular.
     templateUrl: 'user-list/user-list.template.html',
     controller: ['$http', '$scope',  function UserListController($http, $scope) {
         var self = this;
-        self.user = {
-            name : null,
-            phone : null,
-            address : null,
-            picture: "images/user-image.png",
-            userIndex : null
+        
+        $scope.resetUser = function() {
+            self.user = {
+                name : null,
+                phone : null,
+                address : null,
+                picture: "images/user-image.png",
+                userIndex : null
+            }
+            self.header = "Add";
         }
+        
+        $scope.resetUser();
 
         $scope.addUser = function() {
             if(self.user.userIndex != null)
-                self.users[self.user.userIndex] = self.user;
+                self.users[self.user.userIndex] = angular.copy(self.user);
             else
-                self.users.push(self.user);
+                self.users.push(angular.copy(self.user));
         }
         
         $scope.searchUser = function() {
-            self.query = document.getElementById("searchKeyword").value;
+            self.query = document.getElementById("search-keyword").value;
         }
         
         $scope.setUser = function(userIndex) {
-            if(userIndex != null) {
-                self.user.name = self.users[userIndex].name;
-                self.user.phone = self.users[userIndex].phone;
-                self.user.address = self.users[userIndex].address;
-                self.user.userIndex = userIndex;
-            }
-            else {
-                self.user.userIndex = null;
-                self.user.name = null;
-                self.user.phone = null;
-                self.user.address = null;
-            }
+            self.user = self.users[userIndex];
+            self.user.userIndex = userIndex;
+            self.header = "Edit";
         }
-
+        
         $http.get('users/users.json').then(function(response) {
           self.users = response.data;
         });
