b48ef036f7029defc15331daadaf77e15cd2f931
prakalpa rathore <prakalpa.rathore@gmail.com>
2018-10-25 19:07:44 +0530

    UI-Assignment-11

--- UI-Assignment-11/UI-Assignment-11/shipping-details/shipping-details.component.js
+++ UI-Assignment-11/UI-Assignment-11/shipping-details/shipping-details.component.js
@@ -14,5 +14,44 @@ component('shippingDetails', {
                 self.cartTotal += self.cartItems[item].price * self.cartItems[item].count;
             }
         });
+
+        $scope.placeOrder = function () {
+            $rootScope.cartCount = 0;
+            var order = {
+                "customerName": $rootScope.customerName,
+                "date": new Date()
+            }
+
+            $http({
+                method: 'POST',
+                url: 'http://localhost:3000/orders/',
+                data: order,
+                dataType: 'json'
+            });
+
+            $http.get('http://localhost:3000/cart').then(function (response) {
+                self.cartItems = response.data;
+
+                self.cartItems.forEach(item => {
+                    $http.get('http://localhost:3000/items/' + item.id).then(function successCallback(response) {
+                        response.data.count = 0;
+
+                        $http({
+                            method: 'PUT',
+                            url: 'http://localhost:3000/items/' + item.id,
+                            data: response.data,
+                            dataType: 'json'
+                        });
+                    });
+
+                    $http({
+                        method: 'DELETE',
+                        url: 'http://localhost:3000/cart/' + item.id
+                    });
+                });
+            });
+            
+            window.location.href="#!/order-success";
+        }
 }]
 });
